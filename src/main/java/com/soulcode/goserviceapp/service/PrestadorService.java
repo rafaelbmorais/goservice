e6887c2c045ac0eaade1b1cc223da4e546ebb410
rafaelbmorais <morais.rafael@outlook.com>
2023-08-23 11:52:50 -0300

    Construção dos serviços de Prestador e Cliente.

--- src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
+++ src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
@@ -1,4 +1,33 @@
 package com.soulcode.goserviceapp.service;
 
+import com.soulcode.goserviceapp.domain.Prestador;
+import com.soulcode.goserviceapp.repository.PrestadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class PrestadorService {
+
+    @Autowired
+    private PrestadorRepository prestadorRepository;
+
+    public Prestador findById(Long id) {
+        Optional<Prestador> prestador = prestadorRepository.findById(id);
+        if(prestador.isPresent()) { // isPresent faz parte do Optional
+            return prestador.get(); // o get retorna todos os dados do prestador
+        } else {
+            throw new RuntimeException("Prestador não encontrado!"); // tratamento de erro
+        }
+    }
+
+    public Prestador update(Prestador prestador) {
+        Prestador updatedPrestador = this.findById(prestador.getId());
+        updatedPrestador.setNome(prestador.getNome());
+        updatedPrestador.setEmail(prestador.getEmail());
+        updatedPrestador.setDescricao(prestador.getDescricao());
+        updatedPrestador.setTaxaPorHora(prestador.getTaxaPorHora());
+        return prestadorRepository.save(updatedPrestador);
+    }
 }
