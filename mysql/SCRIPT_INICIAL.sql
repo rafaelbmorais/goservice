79cd5184aefaf4126e0c9df496e4d1a62ee73a21
rafaelbmorais <morais.rafael@outlook.com>
2023-08-25 18:27:53 -0300

    Construção da funcionalidade editar serviços.

--- mysql/SCRIPT_INICIAL.sql
+++ mysql/SCRIPT_INICIAL.sql
@@ -13,6 +13,19 @@ VALUE (
     1
 ); # 123456
 
-SELECT * FROM usuarios;
+SELECT * FROM servicos;
 
 update usuarios set habilitado = 0 where id in (3, 4);
+
+INSERT INTO servicos (categoria, descricao, nome) 
+VALUES 
+    ('Limpeza', 'Serviço de limpeza residencial', 'Limpeza Residencial'),
+    ('Treinamento', 'Treinamento corporativo em liderança', 'Treinamento em Liderança'),
+    ('Saúde', 'Consulta médica de clínico geral', 'Consulta Clínico Geral'),
+    ('Desenvolvimento', 'Desenvolvimento de aplicativo móvel', 'Desenvolvimento de App'),
+    ('Design', 'Design de logotipo para empresas', 'Design de Logotipo'),
+    ('Limpeza', 'Limpeza de escritórios comerciais', 'Limpeza Comercial'),
+    ('Treinamento', 'Treinamento em habilidades de comunicação', 'Treinamento em Comunicação'),
+    ('Saúde', 'Sessão de fisioterapia para reabilitação', 'Fisioterapia de Reabilitação'),
+    ('Desenvolvimento', 'Desenvolvimento de site institucional', 'Desenvolvimento de Site Institucional'),
+    ('Design', 'Design de interface de usuário para aplicativos', 'Design de Interface de Aplicativo');
